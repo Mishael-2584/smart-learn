c056f006261f214493e0bc23d493ff18d38bc2a5
Mishael-2584 <worancha2584@student.babcock.edu.ng>
2023-11-28 15:16:53 +0100

    changed asset tag to secure asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,21 +9,23 @@
 <title>Blank Page &mdash; SMARTLEARN </title>
 
 <!-- General CSS Files -->
-<link rel="stylesheet" href="{{ asset("codiepie/assets/modules/bootstrap/css/bootstrap.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/modules/bootstrap/css/bootstrap.min.css") }}">
 {{-- <link rel="stylesheet" href="{{ asset("codiepie/assets/modules/fontawesome/css/all.min.css") }}"> --}}
 
 <!-- CSS Libraries -->
-<link rel="stylesheet" href="{{ asset("codiepie/assets/modules/datatables/datatables.min.css") }}">
-<link rel="stylesheet" href="{{ asset("codiepie/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css") }}">
-<link rel="stylesheet" href="{{ asset("codiepie/assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/modules/datatables/datatables.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css") }}">
 
 {{-- Font Awesome --}}
 
 
 <!-- Template CSS -->
-<link rel="stylesheet" href="{{ asset("codiepie/assets/css/css") }}">
-<link rel="stylesheet" href="{{ asset("codiepie/assets/css/style.min.css") }}">
-<link rel="stylesheet" href="{{ asset("codiepie/assets/css/components.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/css/css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/css/style.min.css") }}">
+<link rel="stylesheet" href="{{ secure_asset("codiepie/assets/css/components.min.css") }}">
+
+
 </head>
 
 <body class="layout-4">
